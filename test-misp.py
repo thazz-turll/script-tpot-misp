8c419cc335ad9990a2cf04a00bfdc4a390c882ef
thazz-turll <tuthanhilap@gmail.com>
2025-08-08 14:30:12 +0700

    Update test-misp.py

--- test-misp.py
+++ test-misp.py
@@ -3,6 +3,8 @@ from pymisp import ExpandedPyMISP
 misp_url = "https://192.168.1.101/"
 misp_key = "hW0DZztasm17mclsC4lkkPgFq6n8JY4ohKRPk4v5"  # key của bạn
 misp_verifycert = False
-
 misp = ExpandedPyMISP(misp_url, misp_key, misp_verifycert)
-print(misp.get_version())
+
+# Cách 1: Gọi hàm server_info() để kiểm tra
+info = misp.server_info()
+print(info)
