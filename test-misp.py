9832334a382f9092e07d06046ed6adaa93880e68
thazz-turll <tuthanhilap@gmail.com>
2025-08-08 14:39:17 +0700

    Update test-misp.py

--- test-misp.py
+++ test-misp.py
@@ -1,4 +1,4 @@
 from pymisp import PyMISP
 
-misp = PyMISP("https://192.168.1.101/", "<API_KEY>", False)
+misp = PyMISP("https://192.168.1.101/", hW0DZztasm17mclsC4lkkPgFq6n8JY4ohKRPk4v5, False)
 print(misp.direct_call('servers/getVersion.json'))
